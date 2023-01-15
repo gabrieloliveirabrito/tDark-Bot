41a6a9f3e508505cd8bb5407a40a62e28abec4b3
Gabrieemail <gabriel.oliveira.brito@outlook.com>
2023-01-14 23:16:58 -0300

    Primeiro Commit

---
+++ DarkBot.cs
@@ -0,0 +1,98 @@
+using System.Reflection;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using tDarkBot.Services;
+
+namespace tDarkBot
+{
+    public class DarkBot
+    {
+        private DiscordSocketClient client;
+        private InteractionService interaction;
+        private CurrencyConverterService currencyConverter;
+
+        public DarkBot()
+        {
+            var config = new DiscordSocketConfig();
+            config.GatewayIntents = GatewayIntents.GuildMembers | GatewayIntents.MessageContent | GatewayIntents.Guilds | GatewayIntents.GuildMessages;
+            config.UseInteractionSnowflakeDate = true;
+            config.UseSystemClock = true;
+            config.AlwaysDownloadUsers = true;
+
+
+            client = new DiscordSocketClient(config);
+            client.Log += LogAsync;
+            client.Ready += ReadyAsync;
+            client.InteractionCreated += InteractionCreated;
+
+            interaction = new InteractionService(client);
+            interaction.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+            currencyConverter = new CurrencyConverterService();
+        }
+
+        public async Task Start()
+        {
+            try
+            {
+                await currencyConverter.MakeCacheAsync();
+
+                await client.LoginAsync(TokenType.Bot, "OTk5MzUxNDU3OTM5NzE4MTU0.GJF6Qz.N4gtWhsRYFj4SPyKSZutGQdzw_zb76PuAsHdio");
+                await client.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private async Task InteractionCreated(SocketInteraction arg)
+        {
+            if (arg != null)
+            {
+                var ctx = new SocketInteractionContext(client, arg);
+                IInteractionContext? botContext;
+
+                switch (arg.Type)
+                {
+                    case InteractionType.ApplicationCommand:
+                        botContext = new DarkBotContext<SocketSlashCommand>(client, arg as SocketSlashCommand);
+                        break;
+                    case InteractionType.MessageComponent:
+                        botContext = new DarkBotContext<SocketMessageCommand>(client, arg as SocketMessageCommand);
+                        break;
+                    case InteractionType.ModalSubmit:
+                        botContext = new DarkBotContext<SocketModal>(client, arg as SocketModal);
+                        break;
+                    default:
+                    case InteractionType.Ping:
+                        botContext = new DarkBotContext<SocketUserCommand>(client, arg as SocketUserCommand);
+                        break;
+                }
+
+                await interaction.ExecuteCommandAsync(botContext, null);
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{client.CurrentUser} is connected!");
+
+            interaction.RegisterCommandsGloballyAsync();
+
+            client.SetActivityAsync(new Game("Bot developed on C# (CSharp)", ActivityType.Playing));
+            client.SetStatusAsync(UserStatus.Online);
+
+            return Task.CompletedTask;
+        }
+    }
+}
